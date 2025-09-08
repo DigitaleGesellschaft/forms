307778bc0c772a929b99334820d3a35fe633afb5
Salim B <git@salim.space>
2025-09-08 18:13:58 +0200

    fix: DB init script

--- init_db.sql
+++ init_db.sql
@@ -1,20 +1,23 @@
 /* Initialize PostgreSQL for Formbricks
 
-   Below commands are intended to be run once before a new Formbricks instance is started for the first time.
+   Below commands are intended to be run once as admin/superuser role before a new Formbricks instance is started for the first time.
 */
  
 -- Switch to `admin` role (errors if not authorized)
-SET ROLE admin;
 
 -- Create dedicated DB
-CREATE DATABASE formbricks WITH OWNER admin;
+CREATE DATABASE formbricks;
+
+-- Configure user for Formbricks
+CREATE ROLE "formbricks" WITH LOGIN PASSWORD 'REPLACE-ME';
+GRANT ALL PRIVILEGES ON DATABASE "formbricks" TO "formbricks";
+
+-- NOTE: everything below must be run while connected to "formbricks" DB!!!
 
 -- Install extensions required by Formbricks
 CREATE EXTENSION IF NOT EXISTS vector;
 
--- Configure user for Formbricks
-CREATE ROLE "formbricks" WITH LOGIN PASSWORD 'REPLACE-ME' ROLE "admin";
-GRANT ALL PRIVILEGES ON DATABASE "formbricks" TO "formbricks";
+-- Grant privileges to Formbricks user
 GRANT ALL PRIVILEGES ON SCHEMA "public" TO "formbricks";
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA "public" TO "formbricks";
 GRANT ALL PRIVILEGES ON ALL ROUTINES IN SCHEMA "public" TO "formbricks";
