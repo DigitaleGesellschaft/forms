93e8e075aa92eaadd1c1845249d0f091e3627f46
Salim B <git@salim.space>
2025-10-03 22:40:40 +0200

    refactor: simplify db init script

--- init_db.sql
+++ init_db.sql
@@ -3,23 +3,13 @@
    Below commands are intended to be run once as admin/superuser role before a new Formbricks instance is deployed for the first time.
 */
 
--- Create dedicated DB
-CREATE DATABASE formbricks;
-
--- Configure user for Formbricks
+-- Create user for Formbricks
 CREATE ROLE "formbricks" WITH LOGIN PASSWORD 'REPLACE-ME';
-GRANT ALL PRIVILEGES ON DATABASE "formbricks" TO "formbricks";
+
+-- Create dedicated DB
+CREATE DATABASE formbricks WITH OWNER "formbricks";
 
 -- NOTE: everything below must be run while connected to the "formbricks" DB (`\c formbricks`)!
 
 -- Install extensions required by Formbricks
 CREATE EXTENSION IF NOT EXISTS vector;
-
--- Grant privileges to Formbricks user
-GRANT ALL PRIVILEGES ON SCHEMA "public" TO "formbricks";
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA "public" TO "formbricks";
-GRANT ALL PRIVILEGES ON ALL ROUTINES IN SCHEMA "public" TO "formbricks";
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA "public" TO "formbricks";
-ALTER DEFAULT PRIVILEGES IN SCHEMA "public" GRANT ALL PRIVILEGES ON SEQUENCES TO "formbricks";
-ALTER DEFAULT PRIVILEGES IN SCHEMA "public" GRANT ALL PRIVILEGES ON ROUTINES TO "formbricks";
-ALTER DEFAULT PRIVILEGES IN SCHEMA "public" GRANT ALL PRIVILEGES ON TABLES TO "formbricks";
